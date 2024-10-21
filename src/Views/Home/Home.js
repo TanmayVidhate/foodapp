0f5bc72af6f73388fbb6ab733c4ebf61dd4518e3
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-10-21 11:16:35 +0530

    add css and elements

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar';
+import styled from 'styled-components';
 function Home() {
     return (
         <>
-            <Navbar/>
-            <div>Home</div>
+            <Navbar />
+            <CardHolder>
+
+            </CardHolder>
         </>
     )
 }
 
 export default Home;
+
+
+const CardHolder = styled.div`
+height: calc(100vh - 170px);
+background-image: url("./banner.png");
+background-size: cover;
+border: 1px solid yellow;
+`;
