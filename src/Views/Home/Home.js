cab5d39649ac88dc0bd0ba3b91d986dbecfe5723
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-10-21 09:49:40 +0530

    add floder and file

---
+++ src/Views/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar';
+function Home() {
+    return (
+        <>
+            <div>Home</div>
+        </>
+    )
+}
+
+export default Home;
