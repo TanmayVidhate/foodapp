05106ed7931f7e954bb133211aab6a4715756b40
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-10-21 11:05:55 +0530

    add elements and css

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import Foodlogo from './image/logo.png'
 function Navbar() {
     return (
         <>
-            <MainContainer>
+            <Container>
                 <TopContainer>
                     <div className='logo'>
                         <img src={Foodlogo} alt='logoIcon' />
@@ -16,7 +16,14 @@ function Navbar() {
                         />
                     </div>
                 </TopContainer>
-            </MainContainer>
+
+                <ButtonContainer>
+                    <Button>All</Button>
+                    <Button>Breakfast</Button>
+                    <Button>Lunch</Button>
+                    <Button>Dinner</Button>
+                </ButtonContainer>
+            </Container>
         </>
     )
 }
@@ -24,7 +31,38 @@ function Navbar() {
 export default Navbar;
 
 
-const MainContainer = styled.div`
- background-color:rgb(50, 51, 52);
+const Container = styled.div`
+ margin:0 auto ;
+ /* border: 1px solid red; */
 `;
-const TopContainer = styled.section``;
+const TopContainer = styled.section`
+display: flex;
+justify-content: space-between;
+align-items: center;
+height: 10vh;
+padding: 1vw;
+
+.search{
+    input{
+        border: 1px solid red;
+        color: white;
+        padding: 0.8vw;
+        font-size: 1.2vw;
+        border-radius:5px;
+        background-color: transparent;
+    }
+}
+`;
+const ButtonContainer = styled.section`
+display: flex;
+justify-content: center;
+gap: 1vw;
+`;
+
+const Button = styled.button`
+    background-color:#ff4343;
+    color: #FFFFFF;
+    border-radius: 5px;
+    padding: 1vw 1vw;
+    border: none;
+`;
