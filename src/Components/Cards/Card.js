0dc8679d25c400917276dc734773454229890b1e
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-10-22 18:17:17 +0530

    css for Cards

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -20,10 +20,16 @@ function Card({ data }) {
                                     {
                                         name
                                     }
+                                    <div className='text'>
+                                        {text}
+                                    </div>
 
+                                    <div className='btn'>
+                                        <button>
+                                            {price}
+                                        </button>
+                                    </div>
                                 </div>
-
-                                
                             </div>
                         </FoodCard>
                     ))
@@ -36,32 +42,50 @@ function Card({ data }) {
 export default Card;
 
 const Cards = styled.div`
-
+ display: flex;
+ flex-wrap: wrap;
+ width: 90%;
+ justify-content: center;
+ align-items: center;
+ /* border: 5px solid black; */
+ margin: 2vw auto;
 `;
 
 const FoodCard = styled.div`
-width: 350px;
-border:1px solid red;
+width: 380px;
+background-color: rgb(67, 66, 62);
+/* border: 1px solid red; */
 font-size: 1vw;
-padding: 1vw;
+padding: 1.1vw;
 margin: 1vw;
 border-radius: 5px;
 font-size: 1vw;
+border-radius: 5px;
 
 .image{
     img{
         width: 10vw;
-
     }
 }
 
 div{
     display: flex;
-
 }
 
 .name{
     display: block;
-    border: 1px solid yellow;
+}
+
+.text{
+    margin-top:1vw ;
+}
+
+.btn{
+    text-align: end;
+    button{
+        padding: .5vw;
+        text-align: end;
+        background-color: red;
+    }
 }
 `;
