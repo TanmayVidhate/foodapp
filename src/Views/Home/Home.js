6b051f90b03b32d10327e5e426ce7acef6a5eb3c
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-10-22 17:22:25 +0530

    add Url

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,14 +1,49 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar';
 import styled from 'styled-components';
-// import BgImg from './image/banner.png'
+import { useEffect, useState } from 'react'
 import Card from '../../Components/Cards/Card.js';
+
+export const BASE_URL = "http://localhost:9000";
+
 function Home() {
+
+    const [error, setErorr] = useState();
+
+    const [data, setdata] = useState();
+
+    const [loading, setloading] = useState();
+
+    useEffect(() => {
+        const Fetchdata = async () => {
+            setloading(true);
+
+            try {
+                const response = await fetch(BASE_URL);
+
+                const json = await response.json();
+
+                setdata(json);
+
+                setloading(false);
+            }
+            catch (Error) {
+                console.log("Unable to fetch data");
+            }
+
+        }
+        Fetchdata();
+    }, [])
+
+    console.log(data)
+    if (error) return <div>{error}</div>
+
+
     return (
         <>
             <Navbar />
             <CardWrapper>
-                <Card />
+                <Card data={data} />
             </CardWrapper>
         </>
     )
