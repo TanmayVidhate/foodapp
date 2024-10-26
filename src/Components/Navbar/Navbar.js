3ea187226a8817723a520fa3adbf4ad8f1da9b73
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-10-26 18:19:43 +0530

    send data to parent

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,36 +3,79 @@ import styled from 'styled-components';
 import Foodlogo from './image/logo.png';
 // import { type } from '@testing-library/user-event/dist/type';
 
-function Navbar(data) {
-    let filterbtns =[
+function Navbar({searchBarData}) {
+
+    const [searchData, setSearchData] = useState("");
+
+    function sendData() {
+        const filter = FoodItemsInfo?.filter((food) => food.name.toLowerCase().includes(searchData.toLowerCase()));
+        searchBarData(searchData, filter);
+        // console.log("navbar data==",searchData);
+    }
+
+    let filterbtns = [
         {
-            name:"All",
-            type:"all"
+            name: "All",
+            type: "all"
         },
         {
-            name:"Breakfast",
-            type:"breakfats"
+            name: "Breakfast",
+            type: "breakfats"
         },
         {
-            name:"Lunch",
-            type:"lunch"
+            name: "Lunch",
+            type: "lunch"
         },
         {
-            name:"Dinner",
-            type:"dinner"
+            name: "Dinner",
+            type: "dinner"
         }
     ];
-    
-    let arr = Object.values(data);
-    const [filterdata, setFilterData] = useState("");
-    const Search = (e) => {
-        const searchvalue = e.target.value;
-        if (searchvalue === "") {
-            setFilterData(null);
+
+    const FoodItemsInfo = [
+        {
+            "name": "Boilded Egg",
+            "price": 10,
+            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            "image": "/images/egg.png",
+            "type": "breakfast"
+        },
+        {
+            "name": "RAMEN",
+            "price": 25,
+            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            "image": "/images/ramen.png",
+            "type": "lunch"
+        },
+        {
+            "name": "GRILLED CHICKEN",
+            "price": 45,
+            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            "image": "/images/chicken.png",
+            "type": "dinner"
+        },
+        {
+            "name": "CAKE",
+            "price": 18,
+            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            "image": "/images/cake.png",
+            "type": "breakfast"
+        },
+        {
+            "name": "BURGER",
+            "price": 23,
+            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            "image": "/images/burger.png",
+            "type": "lunch"
+        },
+        {
+            "name": "PANCAKE",
+            "price": 25,
+            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            "image": "/images/pancake.png",
+            "type": "dinner"
         }
-        const filter = arr?.filter((food) => food.name.toLowerCase().includes(searchvalue.toLowerCase()));
-        setFilterData(filter);
-    }
+    ];
 
     return (
         <>
@@ -43,7 +86,8 @@ function Navbar(data) {
                     </div>
 
                     <div className='search'>
-                        <input onChange={Search}
+                        <input type='search'
+                            onChange={(e) => { setSearchData(e.target.value); sendData() }}
                             placeholder='Search Food...'
                         />
                     </div>
